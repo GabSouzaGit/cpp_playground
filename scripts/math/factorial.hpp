f3ca66af8a3debc81fee283fe3e827a8dbfad7fc
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-10-14 21:59:02 -0300

    :sparkles: Calculo fatorial

---
+++ scripts/math/factorial.hpp
@@ -0,0 +1,16 @@
+#ifndef FACTORIAL_HPP
+    #define FACTORIAL_HPP
+
+    #include <iostream>
+
+    using namespace std;
+
+    long long int factorial(int x){
+        long long int acc = 1;
+        do {
+            acc *= x;
+        }while(--x > 0); // <- Dica muito boa para while-loops semelhantes a for-loops.
+
+        return acc;
+    }
+#endif
