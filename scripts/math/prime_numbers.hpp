60e7f4cc0b2b58261c53a069fff1916a01a7555c
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-10-18 07:53:56 -0300

    :sparkles: Função caçadora de numeros primos O(n)

---
+++ scripts/math/prime_numbers.hpp
@@ -0,0 +1,26 @@
+#ifndef PRIME_NUMBERS_HPP
+    #define PRIME_NUMBERS_HPP
+
+    #include <iostream>
+    #include <vector>
+
+    using namespace std;
+
+    vector<int> prime_numbers(int n){
+        vector<int> numbers = {2, 3, 5, 7};
+
+        if(n <= 0) return numbers;
+
+        for(int x = 2; x <= n; x++){
+            if(x % 2 == 0) continue; 
+            if(x % 3 == 0) continue;
+            if(x % 5 == 0) continue;
+            if(x % 7 == 0) continue;
+
+            numbers.push_back(x);
+        }
+
+        return numbers;
+    }
+
+#endif
