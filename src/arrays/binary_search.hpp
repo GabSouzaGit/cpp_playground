c1d177532a233f31f4e6675accb849bef3afabd2
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-10-23 17:50:32 -0300

    :sparkles: Busca binária

---
+++ src/arrays/binary_search.hpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target) {
+    int start = 0;
+    int middle = floor((nums.size() - 1) / 2);
+    int last = nums.size() - 1;
+    
+    while(true){
+        if(target != nums[middle] && middle == start){
+            if(target < nums[0]) return 0;
+            if(target > nums[last]) return last + 1;
+            if(target < nums[last]) return last;
+
+            return last;
+        }
+        
+        if(target == nums[middle]) break;
+        
+        if(target > nums[middle]) start = middle;
+        if(target < nums[middle]) last = middle;
+        
+        middle = floor((last - start) / 2) + start;
+    }
+    
+    return middle;
+}
