d1f5fa849cfa0ad5314843ce2a41ea53efd1cedd
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-11-01 21:43:06 -0300

    :bug: Corringindo nome de construtores e macro da classe SingleLinkedList.

--- src/data_structures/linked_lists/SingleLinkedList.hpp
+++ src/data_structures/linked_lists/SingleLinkedList.hpp
@@ -1,5 +1,5 @@
-#ifndef LINKED_LIST_HPP
-    #define LINKED_LIST_HPP
+#ifndef SINGLE_LINKED_LIST_HPP
+    #define SINGLE_LINKED_LIST_HPP
 
     #include <iostream>
 
@@ -40,8 +40,8 @@
             }
 
         public:
-            LinkedList(){}
-            LinkedList(T value){
+            SingleLinkedList(){}
+            SingleLinkedList(T value){
                 this->head = new Node<T>(value);
             }
 
